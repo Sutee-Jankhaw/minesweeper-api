dd17bee3d72735c28c11dc6ec53639f4912d51dd
Mark <sutee.j@igeargeek.com>
2026-02-25 17:41:54 +0700

    add score calculate

--- src/controllers/score-controller.js
+++ src/controllers/score-controller.js
@@ -1,7 +1,21 @@
 const db = require('../config/db')
 
 exports.addScore = (req, res) => {
-  const { username, score } = req.body
+  const { username, revealedCell, time } = req.body
+  let multiple = 1
+  if (time<=40){
+    multiple = 4
+  }
+  else if (40 < time <= 100){
+    multiple = 3
+  }
+  else if (100 < time <= 180){
+    multiple = 2
+  }
+  else {
+    multiple = 1
+  }
+  const score = revealedCell*multiple
 
   const sql = 'INSERT INTO score_board (username, score) VALUES (?, ?)'
 
@@ -12,7 +26,7 @@ exports.addScore = (req, res) => {
 }
 
 exports.getScores = (req, res) => {
-  const sql = 'SELECT * FROM score_board ORDER BY score ASC LIMIT 10'
+  const sql = 'SELECT * FROM score_board ORDER BY score DESC LIMIT 10'
 
   db.query(sql, (err, results) => {
     if (err) return res.status(500).json(err)
